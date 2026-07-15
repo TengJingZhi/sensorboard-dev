9e6133cf79ef59534710333babc5483bdd5f95e7
钉小呆Xiaodai. <98793646+Danieltoraji@users.noreply.github.com>
2026-07-15 17:57:28 +0800

    define pin

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
+
+#define viberatorPin1 35
+#define viberatorPin2 36
+#define viberatorPin3 37
+#define viberatorPin4 38
+#define viberatorPin5 39
+#define viberatorPin6 40
+#define viberatorPin7 41
+
+#define laserOutputPin 9
+#define laserPin1 10
+#define laserPin2 11
+#define laserPin3 12
+#define laserPin4 13
+
+void setup(){
+
+}
+
+void loop(){
+
+}
